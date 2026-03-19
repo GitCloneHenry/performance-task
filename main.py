812996c00cc927008d14584abee368bdbbe4890a
GitCloneHenry <hwagner1015@gmail.com>
2026-03-19 11:42:02 -0600

    Did work on the study_set function

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import json
 import os
 
 
-
 class CardNode:
     def __init__(self, a_side: str, b_side: str):
         self.a_side = a_side
@@ -36,23 +35,48 @@ class CardEngine:
         )
 
     @staticmethod
+    def get_input(prefix: str = "") -> str:
+        return input([f"{[prefix]} ", ""][prefix == ""] + "> ")
+
+    @staticmethod
+    def is_valid_file(file_path: str) -> bool:
+        file = open(file_path, "r")
+
+        try:
+            file_data: Dict[str, str] = json.load(file)
+        except (json.JSONDecodeError, ValueError):
+            return False
+
+        if not os.path.isfile(file_path):
+            return False
+
+        if not "set_title" in file_data.keys():
+            return False
+
+        return True
+
+    @staticmethod
     def get_file_path() -> str | None:
         if len(argv) <= 1:
             available_sets: List[str] = listdir(".\\sets")
 
-            if len(available_sets) == 0:
-                print("No available sets in the sets directory.")
+            valid_sets: List[str] = list(
+                filter(
+                    lambda file_name: CardEngine.is_valid_file(f".\\sets\\{file_name}"),
+                    available_sets,
+                )
+            )
+
+            if len(available_sets) == 0 or len(valid_sets) == 0:
+                print("No valid sets in the sets directory.")
                 return None
 
             print("Please choose a set from the list of existing sets:")
 
-            for iteration, file_name in enumerate(available_sets):
+            for iteration, file_name in enumerate(valid_sets):
                 file = open(f".\\sets\\{file_name}", "r")
 
                 file_data: Dict[str, str] = json.load(file)
-
-                if not "set_title" in file_data.keys():
-                    file_data["set_title"] = "Invalid Set"
 
                 print(f"    {iteration + 1}: {file_data["set_title"]}")
 
@@ -60,9 +84,9 @@ class CardEngine:
 
             while True:
                 try:
-                    choice = int(input())
-                    if 1 <= choice <= len(available_sets):
-                        return ".\\sets\\%s" % available_sets[choice - 1]
+                    choice = int(CardEngine.get_input())
+                    if 1 <= choice <= len(valid_sets):
+                        return ".\\sets\\%s" % valid_sets[choice - 1]
                     print(f"Invalid selection, try again.")
                 except ValueError:
                     print("Please enter a number.")
@@ -72,6 +96,11 @@ class CardEngine:
             argv.pop(1)
             return CardEngine.get_file_path()
 
+    @staticmethod
+    def compare_similarity(a: str, b: str) -> float:
+        set_a, set_b = set(a), set(b)
+        return len(set_a.intersection(set_b)) / len(set_a.union(set_b))
+
     def study_set(self) -> None:
         if not self.card_nodes:
             return
@@ -80,7 +109,9 @@ class CardEngine:
 
         while len(nodes_to_study) > 0:
             node = nodes_to_study.pop(randint(0, len(nodes_to_study) - 1))
-            print(node)
+            print(node.a_side)
+            response = CardEngine.get_input()
+            print(self.compare_similarity(node.b_side, response))
 
 
 if __name__ == "__main__":
